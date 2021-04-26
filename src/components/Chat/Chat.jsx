00fa508683c207f6cca9eb0326072bd9db9f2a7e
danyloshyshkovskyi <danik808@gmail.com>
2021-04-26 13:56:52 +0300

    first commit

---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,114 @@
+import { useEffect } from 'react';
+import { useChat } from 'context';
+import { getChats, ChatEngine } from 'react-chat-engine';
+import { LeftRail, ChatToolbar, ChatInput, MessageList } from 'components';
+import { addPerson, getOtherPeople  } from 'react-chat-engine';
+
+export const Chat = () => {
+  const {
+    myChats,
+    setMyChats,
+    chatConfig,
+    selectedChat,
+    selectChatClick,
+    setSelectedChat,
+  } = useChat();
+
+  const selectUser = username => {
+    const authObject = {'Project-ID': '28862875-ac86-4e66-8b82-93720f7715ba', 'User-Name': 'Danik', 'User-Secret': 'R0sKZ582RgPdI8RzxG9cb0fSPo12'}
+    console.log(username)
+  const chatID = 6807
+  const callback = (chatID, data) => console.log(chatID, data)
+
+    addPerson(chatConfig, chatID, username, callback)
+  };
+
+  const selectUser2 = username => {
+    const chatID = 6807
+    const callback = (chatID, data) => console.log(chatID, data)
+
+getOtherPeople(chatConfig, chatID, callback)
+  };
+
+
+  useEffect(() => {
+    console.log('My Chats: ', myChats);
+  }, [myChats]);
+
+  useEffect(() => {
+    console.log('Selected Chat: ', selectedChat);
+  }, [selectedChat]);
+
+
+
+  return (
+    <>
+      {!!chatConfig && (
+        <ChatEngine
+          hideUI={true}
+          userName={chatConfig.userName}
+          projectID={chatConfig.projectID}
+          userSecret={chatConfig.userSecret}
+          onConnect={() => {
+            getChats(chatConfig, setMyChats);
+            selectUser(chatConfig.userName);
+            selectUser2(chatConfig.userName);
+          }}
+          onNewChat={chat => {
+            if (chat.admin.username === chatConfig.userName) {
+              selectChatClick(chat);
+            }
+            setMyChats([...myChats, chat].sort((a, b) => a.id - b.id));
+          }}
+          onDeleteChat={chat => {
+            if (selectedChat?.id === chat.id) {
+              setSelectedChat(null);
+            }
+            setMyChats(
+              myChats.filter(c => c.id !== chat.id).sort((a, b) => a.id - b.id),
+            );
+          }}
+          onNewMessage={(chatId, message) => {
+            if (selectedChat && chatId === selectedChat.id) {
+              setSelectedChat({
+                ...selectedChat,
+                messages: [...selectedChat.messages, message],
+              });
+            }
+            const chatThatMessageBelongsTo = myChats.find(c => c.id === chatId);
+            const filteredChats = myChats.filter(c => c.id !== chatId);
+            const updatedChat = {
+              ...chatThatMessageBelongsTo,
+              last_message: message,
+            };
+            setMyChats(
+              [updatedChat, ...filteredChats].sort((a, b) => a.id - b.id),
+            );
+          }}
+        />
+      )}
+
+      <div className="chat-container">
+        <LeftRail />
+        <div className="current-chat">
+          {selectedChat ? (
+            <div className="chat">
+              <ChatToolbar />
+              <MessageList />
+              <ChatInput />
+            </div>
+          ) : (
+            <div className="no-chat-selected">
+              <img
+                src="/img/pointLeft.png"
+                className="point-left"
+                alt="point-left"
+              />
+              Select A Chat
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
